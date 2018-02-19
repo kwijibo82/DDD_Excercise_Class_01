1b8dfb5229c46135b4cd057a4d0a2094ad5ab8ca
Daniel Mazzini <daniel.mazzini@gmail.com>
2018-02-19 18:41:41 +0100

    Add test

--- Src/EnvioBoundedContext.Domain.Model.UnitTest/PlatformTest.cs
+++ Src/EnvioBoundedContext.Domain.Model.UnitTest/PlatformTest.cs
@@ -1,11 +1,10 @@
-﻿using System.Threading.Tasks;
-using Telerik.JustMock;
-using TestStack.BDDfy;
+﻿using TestStack.BDDfy;
 using Xunit;
 using Xunit2.Should;
 
 namespace EnvioBoundedContext.Domain.Model.UnitTest
 {
+    [Story(AsA = "a user",IWant ="Search envio by id", SoThat ="'cause yo lo valgo")]
     public class PlatformTest
     {
         private Envio _sut;
@@ -13,17 +12,30 @@ namespace EnvioBoundedContext.Domain.Model.UnitTest
         [Fact]
         public void TestCanRun()
         {
-            this.Given(x => x.None())
+            this.Given(x => x.EnvioIsNull())
                 .When(x => x.CreateEnvio())
                 .Then(x => x.EnvioShouldNotBeNull())
                 .BDDfy();
         }
 
+        [Fact]
+        public void TestCanRun2()
+        {
+            this.Given(x => x.None())
+                .When(x => x.CreateEnvio())
+                .Then(x => x.EnvioShouldNotBeNull())
+                .BDDfy();
+        }
         #region Given
 
         private void None()
         {
 
+        }
+
+        private void EnvioIsNull()
+        {
+            _sut = null;
         }
 
         #endregion
@@ -39,6 +51,9 @@ namespace EnvioBoundedContext.Domain.Model.UnitTest
         private void EnvioShouldNotBeNull()
         {
             _sut.ShouldNotBeNull();
+            
+            //Assert.NotNull(_sut);
+            
         }
         #endregion
 
