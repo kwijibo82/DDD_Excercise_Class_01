4aff6673106a18431ec18639bc15245ed4c79044
Xavi Mora <xamorach@gmail.com>
2018-02-21 19:38:46 +0100

    Servicio class improved

--- Src/EnvioBoundedContext.Domain.Model/Servicio.cs
+++ Src/EnvioBoundedContext.Domain.Model/Servicio.cs
@@ -8,13 +8,9 @@ namespace EnvioBoundedContext.Domain.Model
 {
     class Servicio
     {
-        // TODO: Tanto las entidades como los VO tienen propiedades readonly
-        //Politica id. Primero Entidad luego sufijo 
-        public Guid IDPolitica { get; set; }
-
-        //TODO: Es solo una propiedad. TiempoEstimadoEntrega
-        public string TiempoEstimadp { get; set; }
-        public string TiempoEntrega { get; set; }
-        public string Nombre { get; set; }
+        // Tanto las entidades como los VO tienen propiedades readonly
+        public Guid PoliticaID { get; }
+        private static string TiempoEstimadoEntrega { get; }
+        public string Nombre { get; }
     }
 }
