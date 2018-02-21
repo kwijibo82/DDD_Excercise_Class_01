2b0213b8e78e87847bb4043fd2ca7807149bc4f0
Raul <corchero100@gmail.com>
2018-02-21 19:11:18 +0100

    Creada clase Requires y actualizada la clase Dirección

--- Src/EnvioBoundedContext.Domain.Model/Direccion.cs
+++ Src/EnvioBoundedContext.Domain.Model/Direccion.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace EnvioBoundedContext.Domain.Model
 {
-    // TODO: public en la clase
-    public class Direccion : ValueObject<Direccion>, IEquatable<Direccion>
+    public class Direccion : ValueObject<Direccion>
     {
         public string TipoVia { get; }
         public string NombreCalle { get; }
@@ -19,18 +18,18 @@ namespace EnvioBoundedContext.Domain.Model
         public string Localidad { get; }
         public string Provincia { get; }
 
-        // TODO: Nomenclatura y validaciones
+
         public Direccion(string tipoVia, string nombreCalle, string numeroPortal, string piso, string puerta, string escalera, string codigoPostal, string localidad, string provincia)
         {
-            if (string.IsNullOrWhiteSpace(tipoVia)) throw new ArgumentNullException(nameof(tipoVia));
-            if (string.IsNullOrWhiteSpace(nombreCalle)) throw new ArgumentNullException(nameof(nombreCalle));
-            if (string.IsNullOrWhiteSpace(numeroPortal)) throw new ArgumentNullException(nameof(numeroPortal));
-            if (string.IsNullOrWhiteSpace(piso)) throw new ArgumentNullException(nameof(piso));
-            if (string.IsNullOrWhiteSpace(puerta)) throw new ArgumentNullException(nameof(puerta));
-            if (string.IsNullOrWhiteSpace(escalera)) throw new ArgumentNullException(nameof(escalera));
-            if (string.IsNullOrWhiteSpace(codigoPostal)) throw new ArgumentNullException(nameof(codigoPostal));
-            if (string.IsNullOrWhiteSpace(localidad)) throw new ArgumentNullException(nameof(localidad));
-            if (string.IsNullOrWhiteSpace(provincia)) throw new ArgumentNullException(nameof(provincia));
+            Requires.NotNullOrEmpty(tipoVia, nameof(tipoVia));
+            Requires.NotNullOrEmpty(nombreCalle, nameof(nombreCalle));
+            Requires.NotNullOrEmpty(numeroPortal, nameof(numeroPortal));
+            Requires.NotNullOrEmpty(piso, nameof(piso));
+            Requires.NotNullOrEmpty(puerta, nameof(puerta));
+            Requires.NotNullOrEmpty(escalera, nameof(escalera));
+            Requires.NotNullOrEmpty(codigoPostal, nameof(codigoPostal));
+            Requires.NotNullOrEmpty(localidad, nameof(localidad));
+            Requires.NotNullOrEmpty(provincia, nameof(provincia));
 
             this.TipoVia = tipoVia;
             this.NombreCalle = nombreCalle;
