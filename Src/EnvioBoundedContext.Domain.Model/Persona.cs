6dbe178e3ded33ca90febf6e966d52c14def2319
Daniel Mazzini <daniel.mazzini@gmail.com>
2018-02-21 20:37:28 +0100

    refactor persona

--- Src/EnvioBoundedContext.Domain.Model/Persona.cs
+++ Src/EnvioBoundedContext.Domain.Model/Persona.cs
@@ -5,63 +5,23 @@ namespace EnvioBoundedContext.Domain.Model
     /// <summary>
     /// Entidad Persona
     /// </summary>
-    public class Persona : IEquatable<Guid>
+    public class Persona : IEquatable<Persona>
     {
         // 1. Internal variables
-        private Guid _id;
+        private Guid _id { get; set; }
         private string _nombre;
         private string _apellido1;
         private string _apellido2;
-        
+
         // 2. Public Properties
         // Tanto las entidades como los VO tienen propiedades readonly
-        public Guid Id
-        {
-            get
-            {
-                return _id;
-            }
-            private set
-            {
-                _id = value;
-            }
-        }
+        public Guid Id => _id;
 
-        public string Nombre
-        {
-            get
-            {
-                return _nombre;
-            }
-            private set
-            {
-                _nombre = value;
-            }
-        }
-        
-        public string Apellido1
-        {
-            get
-            {
-                return _apellido1;
-            }
-            private set
-            {
-                _apellido1 = value;
-            }
-        }
+        public string Nombre => _nombre;
 
-        public string Apellido2
-        {
-            get
-            {
-                return _apellido2;
-            }
-            private set
-            {
-                _apellido2 = value;
-            }
-        }
+        public string Apellido1 => _apellido1;
+
+        public string Apellido2 => _apellido2;
 
 
         // 3. Constructor
@@ -71,26 +31,20 @@ namespace EnvioBoundedContext.Domain.Model
             Requires.NotNullOrEmpty(apellido1, nameof(apellido1));
             Requires.NotNullOrEmpty(apellido2, nameof(apellido2));
 
-            Id = Guid.NewGuid();
-            Nombre = nombre;
-            Apellido1 = apellido1;
-            Apellido2 = apellido2;
+            _id = Guid.NewGuid();
+            _nombre = nombre;
+            _apellido1 = apellido1;
+            _apellido2 = apellido2;
         }
 
 
         // 4. Public methods
-        public string ObtenerNombreCompleto()
-        {
-            var nombreCompleto = string.Format("{0} {1} {2}", Nombre, Apellido1, Apellido2) ;
-            return nombreCompleto;
-        }
+        public string ObtenerNombreCompleto() => $"{Nombre} {Apellido1} {Apellido2}";
 
-        public bool Equals(Guid otroId)
+        public bool Equals(Persona otraPersona)
         {
-            return ((IEquatable<Guid>)Id).Equals(otroId);
+            return otraPersona.Id.Equals(Id);
         }
-        
-
         // 5. Private methods
 
 
